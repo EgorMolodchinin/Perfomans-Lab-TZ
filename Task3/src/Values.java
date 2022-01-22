0c61cbd2cfaf56a3fc9329c3a92feaffb4abbb44
Egor Molodchinin <88039137+EgorMolodchinin@users.noreply.github.com>
2022-01-23 01:03:57 +0300

    Add files via upload

---
+++ Task3/src/Values.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Values {
+	private List<OneValue> values;
+
+	public List<OneValue> getValues() {
+		return values;
+	}
+
+	public void setValues(List<OneValue> values) {
+		this.values = values;
+	}
+
+	@Override
+	public String toString() {
+		return "values: " + values;
+	}	
+}
